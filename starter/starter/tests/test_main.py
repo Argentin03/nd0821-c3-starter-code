1a3860d1e3dff7d5150a214e89f217099e35af67
Rodrigo Argentin <rargentin@Remsoft.local>
2025-02-09 18:41:17 -0400

    Adjustments to run pytest without errors.

--- starter/starter/tests/test_main.py
+++ starter/starter/tests/test_main.py
@@ -1,4 +1,6 @@
 from fastapi.testclient import TestClient
+import sys
+import os
 from main import app
 
 # Create a test client
@@ -64,3 +66,7 @@ def test_post_predict_low_income():
     assert response.json()["prediction"] == "<=50K", (
         f"Expected '<=50K' got {response.json()['prediction']}"
     )
+
+
+if __name__ == "__main__":
+    sys.path.append(os.path.abspath("../../"))
